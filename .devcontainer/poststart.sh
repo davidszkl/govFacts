12c56025400e8ba771211b47eb182c62471a6fe3
Fluffy_Kaiju <contact@dreemcloud.net>
2024-12-02 22:58:50 +0000

    build(devcontainer): update poststart script
    * Add swag ascii art to bashrc
    * Print url of dbgate service to bash

--- .devcontainer/poststart.sh
+++ .devcontainer/poststart.sh
@@ -1,1 +1,7 @@
+echo "cat /workspaces/.devcontainer/swag" >> ~/.bashrc
+
+echo "echo dbgate: http://localhost:3030" >> ~/.bashrc
+
+pip install -r /workspaces/app/requirements.txt
+
 echo "Hello World! ~UwU~"
